f75c6271fc05cbef798acaa8750d31b42daadd29
samineses <163475136+samineses@users.noreply.github.com>
2024-04-20 03:20:03 -0300

    Atualizar o CampoDeBatalha.java

--- battleClass/CampoDeBatalha.java
+++ battleClass/CampoDeBatalha.java
@@ -10,27 +10,35 @@ public class CampoDeBatalha {
 
     public static Resultado batalharContraMonstro(Monstro monstro, Player player){
         
-        if(player.playerPower()>monstro.getPoder()){
+        if(player.playerPower()>=monstro.getPoder()){
             print("Seu poder é maior que o do monstro  - Você Venceu!");
             for (int i=0; i<monstro.getTesouros().size(); i++){
                 player.getInventory().adicionarItem(monstro.getTesouros().get(i));
             }
             return Resultado.VITORIA;
         }
-        else{
-            //try to escape
+        if(player.playerPower()<monstro.getPoder()){
             String s = "Seu poder é menor que o do monstro!"+
                         player.getNAME() + " lança um dado" ;
             print(s);
-
+            
             Random dado = new Random();
             int p = dado.nextInt(6)+1;
-            
             if (p == 5 || p==6){
                 print("dado = "+ p +" , but could scape");
                 print("You run away");
                 return Resultado.FUGA;
             }
+
+        }
+        else{
+            //try to escape
+            
+            
+
+            
+            
+            
             
             else{
                 print("dado = "+ p +" , but could not scape");                
